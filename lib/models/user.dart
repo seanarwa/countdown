af6a580264366df458ea8d64addb47f9980a8b50
Sean Chang <seanarwa@gmail.com>
2019-09-23 10:13:38 -0700

    Added basic schema for User model

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+//TABLE User {
+//id: ID!
+//displayName: String!
+//email: String!
+//photoUrl: String
+//}
+
+class User {
+  String id;
+  String displayName;
+  String email;
+  String photoUrl;
+}
