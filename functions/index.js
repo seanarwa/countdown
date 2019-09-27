e98397185b0081977866e5c436690835d7453f66
Sean Chang <seantc@uci.edu>
2019-09-27 09:52:52 -0700

    Added comments for cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+// initialize admin and db instance
 admin.initializeApp();
 const db = admin.database();
 
+// Constants
 const YEAR_IN_SECONDS = 31536000; //assuming 365 days a year, not 365.25
+
+// Parameters for delivering death time at different probabilities and threshold
 const THRESHOLD_MIN = 120; // 2 minutes
 const THRESHOLD_MAX = 60 * Number(YEAR_IN_SECONDS); // 60 years
 const THRESHOLDS = [
@@ -16,6 +20,7 @@ const THRESHOLDS = [
     THRESHOLD_MAX,
 ];
 
+// Cloud function entrypoints
 exports.onUserCreate = functions.database.ref('/user/{userId}')
     .onCreate(async (snap, context) => {
         const userId = context.params.userId;
@@ -24,19 +29,25 @@ exports.onUserCreate = functions.database.ref('/user/{userId}')
         return snap.ref.child('deathTime').set(deathTime);
     });
 
+// generate a random death time in seconds by defined threshold
 function generateDeathTimeInSeconds() {
 
+    // set initial values of the min max values of thresholds
     var min = THRESHOLD_MIN;
     var max = THRESHOLD_MAX;
 
+    // randomly select a threshold
     var thresholdIdx = randomNumberBetween(0, 4);
 
+    // get min max values for the selected threshold
     min = THRESHOLDS[thresholdIdx];
     max = THRESHOLDS[thresholdIdx + 1];
 
+    // return a random number within the selected threshold
     return randomNumberBetween(min, max);
 }
 
+// generates a random number between num1 and num2
 function randomNumberBetween(num1, num2) {
     return Math.floor(Math.random() * num2) + num1;
 }
